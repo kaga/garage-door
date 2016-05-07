2894d424d617528171043d92f0bb177099f27225
kaga <kaga21@gmail.com>
2016-05-07 12:15:03 +0000

    add nodejs sevice

---
+++ node/index.js
@@ -0,0 +1,23 @@
+var Gpio = require('onoff').Gpio;
+var express = require('express');
+
+var app = express();
+app.get('/v1/garage/toggle/', function(request, response) {
+	toggleGarageDoor();
+	response.send('successfull');
+});
+
+app.listen(3000, function() {
+	console.log("Done");
+})
+
+
+
+function toggleGarageDoor() {
+	var garageRelay = new Gpio(17, 'out low');
+
+	garageRelay.writeSync(1);
+	setTimeout(function() {
+		garageRelay.writeSync(0);
+	}, 1000);
+}
---
+++ notes.md
@@ -0,0 +1,4 @@
+Connect Pi to a network
+	if connect via wifi to aiport extreme, set wifi channel to a fix one or the pi will not be reached
+Install Bonjour
+	$ sudo apt-get install libnss-mdns
