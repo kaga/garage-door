0d71c3bd599801e2101d79680d0a34d573f5f9ce
kaga <kaga21@gmail.com>
2016-05-25 21:16:57 +1200

    test bluetooth to trigger light

--- node/src/index.ts
+++ node/src/index.ts
@@ -2,6 +2,7 @@ import express = require('express');
 import IRelay = require('./gpio/IRelay');
 import RaspberryPiRelay = require('./gpio/RaspberryPiRelay');
 var Gpio = require('onoff').Gpio;
+var noble = require('noble');
 
 var app = express();
 app.get('/v1/ping', require('./route/ping'));
@@ -17,6 +18,7 @@ var switchLight = {
 }
 var garageRelay = new Gpio(switchLight.gpioOutputPin, 'out');
 var switchLightRelay = new IRelay.DebounceRelay(switchLight, (state) => {
+	console.log('Switch Light: ' + state ? "On": "Off" );
 	garageRelay.writeSync(state ? 1 : 0);
 });
 
@@ -28,4 +30,23 @@ app.get('/v1/garage/toggle/', (request, response) => {
 
 app.listen(3000, function () {
 	console.log("Done");
+});
+
+var RSSI_THRESHOLD = -46; //~2m
+
+noble.on('discover', function (peripheral) {
+	if (peripheral.rssi < RSSI_THRESHOLD) {
+		// ignore
+		return;
+	}
+
+	switchLightRelay.switchOn();
+});
+
+noble.on('stateChange', function (state) {
+	if (state === 'poweredOn') {
+		noble.startScanning([], true);
+	} else {
+		noble.stopScanning();
+	}
 });
