4953ff60e55024a5964769dcaac77c48871f4e50
kaga <kaga21@gmail.com>
2016-09-11 13:41:09 +1200

    add measuredPower to ble init parameters

--- ibeacon/index.js
+++ ibeacon/index.js
@@ -2,14 +2,14 @@ var bleno = require('bleno');
 
 var uuid = '841400cb-1a95-4e34-bfbf-282c7b1efc99';
 var major = 1; // 0x0000 - 0xffff
-var minor = 0; // 0x0000 - 0xffff
+var minor = 1; // 0x0000 - 0xffff
 var measuredPower = -59; // -128 - 127
 
 bleno.on('stateChange', function(state) {
   console.log('on -> stateChange: ' + state);
 
   if (state === 'poweredOn') {
-    bleno.startAdvertisingIBeacon(uuid, major, minor);
+    bleno.startAdvertisingIBeacon(uuid, major, minor, measuredPower);
   }
 });
 
